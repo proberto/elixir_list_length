0e0bc020ef858237342cae5ab1462c1167ad111a
Paulo Roberto <paulo.araujo@nhecotech.com>
2021-10-04 06:38:35 -0300

    projeto tamanho da lista

---
+++ lib/elixir_list_length.ex
@@ -0,0 +1,10 @@
+defmodule ElixirListLength do
+  def call(list), do: length(list, 0)
+
+  defp length([], acc), do: acc
+
+  defp length([head | tail], acc) do
+    acc = acc + 1
+    length(tail, acc)
+  end
+end
