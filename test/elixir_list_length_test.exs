0717d3d45bc38f213d053882c6e06fa388448a2a
Paulo Roberto <paulo.araujo@nhecotech.com>
2021-10-04 06:52:41 -0300

    ajustes

--- test/elixir_list_length_test.exs
+++ test/elixir_list_length_test.exs
@@ -7,15 +7,15 @@ defmodule ElixirListLengthTest do
       list2 = [1, 2]
       list3 = [1, 2, 3, 4]
 
-      assert ListLength.call(list1) == 6
-      assert ListLength.call(list2) == 2
-      assert ListLength.call(list3) == 4
+      assert ElixirListLength.call(list1) == 6
+      assert ElixirListLength.call(list2) == 2
+      assert ElixirListLength.call(list3) == 4
     end
 
     test "passing an empty list" do
       list = []
 
-      assert ListLength.call(list) == 0
+      assert ElixirListLength.call(list) == 0
     end
   end
 end
